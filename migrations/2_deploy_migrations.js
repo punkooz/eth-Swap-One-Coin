c71905b572feb8cc1f36f6de2fd43dcf469d1f9e
Samrat Das <oozsober@gmail.com>
2020-03-10 21:54:01 +0530

    eth-Swap, Token deployed on ganache

---
+++ migrations/2_deploy_migrations.js
@@ -0,0 +1,17 @@
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function(deployer) {
+
+    //Deploy Token
+    await deployer.deploy(Token);
+    const token = await Token.deployed()
+
+    //Deploy Eth Swap
+    await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed()
+
+    //Transfer all tokens to EthSwap contract
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+
+};
