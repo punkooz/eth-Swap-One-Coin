bfda8686dd02f55413755e6e3b6567c90986ca3b
Samrat Das <oozsober@gmail.com>
2020-03-21 01:47:37 +0530

    Updates

---
+++ precache-manifest.1ceca86e7c201fb6c8d952bd8b81762f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183d866c2d670fc5f45a6ff61f173d2b",
+    "url": "/eth-Swap-One-Coin/index.html"
+  },
+  {
+    "revision": "c6d4eeade4d9b9972ca5",
+    "url": "/eth-Swap-One-Coin/static/css/2.85f22f3a.chunk.css"
+  },
+  {
+    "revision": "f053740f69774e7799fb",
+    "url": "/eth-Swap-One-Coin/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "c6d4eeade4d9b9972ca5",
+    "url": "/eth-Swap-One-Coin/static/js/2.85a780ff.chunk.js"
+  },
+  {
+    "revision": "bd48e7aad8ffbde0152bdbc9a20111c5",
+    "url": "/eth-Swap-One-Coin/static/js/2.85a780ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f053740f69774e7799fb",
+    "url": "/eth-Swap-One-Coin/static/js/main.c816f49c.chunk.js"
+  },
+  {
+    "revision": "a303f5dc5c55d430436c",
+    "url": "/eth-Swap-One-Coin/static/js/runtime-main.fdc8ad27.js"
+  },
+  {
+    "revision": "766c7acad60c795e408a4d5b51c9bd74",
+    "url": "/eth-Swap-One-Coin/static/media/memecoin.766c7aca.png"
+  }
+]);
